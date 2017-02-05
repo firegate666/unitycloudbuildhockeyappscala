b6b527c65b62f3404cde1006d62db56d2ae58315
Marco Behnke <marco@behnke.biz>
2017-02-05 05:14:24 +0100

    example akka http servers

--- src/main/scala/de/firegate/http/WebServer.scala
+++ src/main/scala/de/firegate/http/WebServer.scala
@@ -1,12 +1,15 @@
+package de.firegate.http
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+
 import scala.io.StdIn
 
 object WebServer {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
 
     implicit val system = ActorSystem("my-system")
     implicit val materializer = ActorMaterializer()
